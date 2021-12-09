abe1ba09acba9c5f6d849b74bca150e13882a535
Christian Lentfort <christian.lentfort@cambridge-intelligence.com>
2021-12-09 13:50:20 +0000

    change config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,6 @@
 module.exports = {
   "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/!(breaks)/story.js"
   ],
   "addons": [
     "@storybook/addon-links",
